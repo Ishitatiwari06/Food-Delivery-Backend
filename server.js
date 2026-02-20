9c9ca90212aaa60725ed2ae4c0d642a005822e4f
Ishita Tiwari <ishitatiwari060@gmail.com>
2026-02-20 20:06:42 +0530

    feat: setup mongodb connection with dotenv configuration

---
+++ server.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./src/app.js";
+import http from "http";
+import connectDB from "./src/config/db.js";
+
+const PORT = process.env.PORT || 5000;
+const server=http.createServer(app);
+connectDB();
+server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
