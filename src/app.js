a18bd574df3237f2d81d2c1a92dea735235a6673
Ishita Tiwari <ishitatiwari060@gmail.com>
2026-02-21 19:27:58 +0530

    feat: implement registration validation middleware using express-validator

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+})); //Required for frontend-backend communication
+
+app.use(express.urlencoded({ extended: true, limit: "10mb" })); //it allows server to read data from req. Without this req.body will be undefined
 app.use("/api", authRoutes);
 export default app;
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { createUser } from "../controllers/auth.controller.js";
+import { validateUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/createuser", createUser);
+router.post("/createuser",validateUser, createUser);
 
 export default router;
