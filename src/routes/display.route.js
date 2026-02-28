0346cd823b6e071b3120bdc2b64c8286b7fd671a
Ishita Tiwari <ishitatiwari060@gmail.com>
2026-02-28 23:24:22 +0530

    added get req for foodData

--- src/routes/display.route.js
+++ src/routes/display.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { getFoodData } from "../controllers/display.controller.js";
 
 const router = express.Router();
-router.post("/foodData",getFoodData)
+router.post("/foodData", getFoodData)
+router.get("/foodData", getFoodData)
 
 export default router;
